19466070afc32a7fe370b1c25d4a633001754ff1
wintershoes <1031132796@qq.com>
2025-09-28 22:58:59 +0800

    add my_carto_config folder with configs

---
+++ my_carto_config/cmake/my_carto_config-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(my_carto_config_MSG_INCLUDE_DIRS "/home/winter/robot_ws/devel/share/my_carto_config/msg")
+set(my_carto_config_MSG_DEPENDENCIES actionlib_msgs;std_msgs)
